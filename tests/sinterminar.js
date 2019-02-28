234f397f1cb91b40b85da2002c25e72548e9ea5d
Julian Zaltron <zaltron.julian@gmail.com>
2019-02-28 09:49:31 -0300

    big-bang

---
+++ tests/sinterminar.js
@@ -0,0 +1,7 @@
+describe('Test Radio Buttons Pafe', function() {
+    browser.url("Dropdown-Checkboxes-RadioButtons/index.html");
+
+    it('Should be able to focus on radio button elements', function(done) {
+
+    });
+});
